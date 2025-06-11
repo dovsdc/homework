259fcf65a53d6c0369592d4ddd00ba3bb9ddd580
dovsdc <natalia.polibza@e-u.edu.ua>
2025-06-11 15:33:57 +0300

    Update hw06.rs

--- src/homework/hw06.rs
+++ src/homework/hw06.rs
@@ -1,16 +1,21 @@
-fn main() {
-  const LEVELS: usize = 5;
- let mut tree = String::new();
-
-  let max_width = 2 * LEVELS + 1;
-
-for level in 1..=LEVELS {
-        for row in 1..=level {
-            let stars = "*".repeat(2 * row - 1);
-            let spaces = " ".repeat((max_width - stars.len()) / 2);
-            tree.push_str(&format!("{}{}\n", spaces, stars));
+pub fn draw_tree(levels: usize) {
+    let max_stars = 1 + 2 * levels; // кількість зірок у найширшому рядку (останній трикутник)
+    
+    for level in 1..=levels {
+        for row in 0..=level {
+            let stars = 1 + 2 * row;
+            let padding = (max_stars - stars) / 2;
+            let line = " ".repeat(padding) + &"*".repeat(stars);
+            println!("{}", line);
         }
     }
 
-    print!("{}", tree);
+    // Додатковий рядок основи
+    let trunk = "*".repeat(max_stars);
+    println!("{}", trunk);
+}
+
+fn main() {
+    let levels = 5; // змінюй тут кількість трикутників
+    draw_tree(levels);
 }
