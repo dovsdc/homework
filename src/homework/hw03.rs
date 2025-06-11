34b4a0dc511f0e5cafee59eec29bbfe14d14827b
dovsdc <natalia.polibza@e-u.edu.ua>
2025-06-11 15:21:17 +0300

    Update hw03.rs

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,29 +1,23 @@
-fn charpix(x: usize, y: usize, width: usize, height: usize) -> char {
-    let border = x == 0 || x == width - 1 || y == 0 || y == height - 1;
-  
-    let lposition = (y * (width - 1)) / (height - 1);
-    let rposition = ((height - 1 - y) * (width - 1)) / (height - 1);
-
-    let diagonal = x == lposition || x == rposition;
-    
-    if border || diagonal {
-        '*'
-    } else {
-        ' '
-    }
-}
+const HEIGHT: usize = 14;
+const WIDTH: usize = 28;
 
 fn main() {
-    const WIDTH: usize = 28;
-    const HEIGHT: usize = 10;
-    
-    let mut output = String::new();
-    
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            output.push(charpix(x, y, WIDTH, HEIGHT));
+    for row in 0..HEIGHT {
+        for col in 0..=WIDTH {
+            let ch = if row == 0 || row == HEIGHT - 1 {
+                // Верхня та нижня рамка
+                '*'
+            } else if col == 0 || col == WIDTH {
+                // Ліва та права рамка
+                '*'
+            } else if col == row * 2 || col == WIDTH - row * 2 {
+                // Діагоналі, які формують конверт
+                '*'
+            } else {
+                ' '
+            };
+            print!("{}", ch);
         }
-        output.push('\n');
+        println!();
     }
-    print!("{}", output);
 }
